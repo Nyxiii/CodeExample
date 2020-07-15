760fe2ed607a27eb892565490b74424e97516f21
Nyxiii <96304160+Nyxiii@users.noreply.github.com>
2020-07-15 15:05:49 +0000

    Edited readme.

---
+++ AnyListen/Designer/Data/IPreviewable.cs
@@ -0,0 +1,11 @@
+
+﻿using AnyListen.Designer.Data.ThemeData;
+
+namespace AnyListen.Designer.Data
+{
+    public interface IPreviewable
+    {
+        AppThemeData AppThemeData { get; }
+        AccentColorData AccentColorData { get; }
+    }
+}
---
+++ AnyListen/Designer/Data/ISaveable.cs
@@ -0,0 +1,10 @@
+
+﻿namespace AnyListen.Designer.Data
+{
+    public interface ISaveable
+    {
+        void Save(string path);
+        string Filter { get; }
+        string BaseDirectory { get; }
+    }
+}
