135f0ed7b367ea5e4274be3ea80136764ea33151
Nyxiii <96304160+Nyxiii@users.noreply.github.com>
2020-07-18 12:18:02 +0000

    grader engine

--- AnyListen/Designer/Data/ThemeColor.cs
+++ AnyListen/Designer/Data/ThemeColor.cs
@@ -12,4 +12,27 @@ namespace AnyListen.Designer.Data
             set
             {
                 _color = value;
-                if (ValueChanged != null) ValueChanged(this, Ev
+                if (ValueChanged != null) ValueChanged(this, EventArgs.Empty);
+            }
+        }
+
+        public string RegexPattern { get; set; }
+        public bool IsTransparencyEnabled { get; set; }
+
+        public ThemeColor()
+        {
+            Color = Color.FromArgb(255, 0, 0, 0);
+            IsTransparencyEnabled = true;
+        }
+
+        public string DisplayName { get; set; }
+
+        public string ID { get; set; }
+
+        public string Value => ColorToString(Color, IsTransparencyEnabled);
+
+        private string ColorToString(Color c, bool withTransparencyValue = true)
+        {
+            return withTransparencyValue ? $"#{c.A:X2}{c.R:X2}{c.G:X2}{c.B:X2}"
+                : $"{c.R:X2}{c.G:X2}{c.B:X2}";
+        }
