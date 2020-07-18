35a8ec2257f09ba3b24311ebe19149fd788a050e
Nyxiii <96304160+Nyxiii@users.noreply.github.com>
2020-07-18 06:03:41 +0000

    maintenance

---
+++ AnyListen/Designer/Data/ThemeColor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Windows.Media;
+
+namespace AnyListen.Designer.Data
+{
+    public class ThemeColor : IThemeSetting
+    {
+        private Color _color;
+        public Color Color
+        {
+            get { return _color; }
+            set
+            {
+                _color = value;
+                if (ValueChanged != null) ValueChanged(this, Ev
