adc0c1708cf520a46a8d781eb3fdc1937c3161fe
Nyxiii <96304160+Nyxiii@users.noreply.github.com>
2020-08-03 19:30:31 +0000

    foo

--- AnyListen/Designer/Data/ThemeData/DataThemeBase.cs
+++ AnyListen/Designer/Data/ThemeData/DataThemeBase.cs
@@ -9,4 +9,23 @@ namespace AnyListen.Designer.Data.ThemeData
 {
     public abstract class DataThemeBase : ISaveable
     {
-        public List<IThemeSetting> ThemeSettings { get;
+        public List<IThemeSetting> ThemeSettings { get; set; }
+        public string Name { get; set; }
+        public abstract string Source { get; }
+        public abstract string Filter { get; }
+        public abstract string BaseDirectory { get; }
+
+        public void LoadFromFile(string filePath)
+        {
+            LoadFromResourceDictionary(new ResourceDictionary {Source = new Uri(filePath)});
+        }
+
+        public void LoadFromResourceDictionary(ResourceDictionary dictionary)
+        {
+            foreach (var setting in ThemeSettings)
+            {
+                setting.SetValue(GetValueFromDictionary(setting.ID, dictionary).ToString());
+            }
+        }
+
+        private object GetValueFromDictionary(string key, Resourc
