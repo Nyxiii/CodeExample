9137eca4f5ff831ccdb2c9a81f4c412bac6f9329
Nyxiii <96304160+Nyxiii@users.noreply.github.com>
2020-09-16 08:18:00 +0000

    Cambios varios

--- AnyListen/Designer/DesignerWindow.xaml.cs
+++ AnyListen/Designer/DesignerWindow.xaml.cs
@@ -2,4 +2,12 @@
 {
     /// <summary>
     /// Interaction logic for DesignerWindow.xaml
-    /// <
+    /// </summary>
+    public partial class DesignerWindow
+    {
+        public DesignerWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
