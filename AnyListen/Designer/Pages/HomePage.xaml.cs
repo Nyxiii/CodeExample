96189c68f2464cc27a42b440ea82ab7708e14033
Nyxiii <96304160+Nyxiii@users.noreply.github.com>
2020-09-17 15:03:16 +0000

    netback: correct netbk_tx_err to handle wrap around.

---
+++ AnyListen/Designer/Pages/HomePage.xaml.cs
@@ -0,0 +1,16 @@
+
+﻿using System.Windows.Controls;
+
+namespace AnyListen.Designer.Pages
+{
+    /// <summary>
+    /// Interaction logic for Home.xaml
+    /// </summary>
+    public partial class HomePage : UserControl
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ AnyListen/Designer/Pages/LivePreview.xaml.cs
@@ -0,0 +1,16 @@
+
+﻿using System.Windows.Controls;
+
+namespace AnyListen.Designer.Pages
+{
+    /// <summary>
+    /// Interaction logic for LivePreview.xaml
+    /// </summary>
+    public partial class LivePreview : UserControl
+    {
+        public LivePreview()
+        {
+            InitializeComponent();
+        }
+    }
+}
