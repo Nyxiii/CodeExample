44e834261d9af67343f1cfddd12b21d2b7c545bb
Nyxiii <96304160+Nyxiii@users.noreply.github.com>
2020-09-23 15:55:31 +0000

    Fix syntax error

--- AnyListen/DragDrop/DropTargetAdorners/DropTargetInsertionAdorner.cs
+++ AnyListen/DragDrop/DropTargetAdorners/DropTargetInsertionAdorner.cs
@@ -9,4 +9,21 @@ namespace AnyListen.DragDrop.DropTargetAdorners
     public class DropTargetInsertionAdorner : DropTargetAdorner
     {
         public DropTargetInsertionAdorner(UIElement adornedElement)
-            : b
+            : base(adornedElement)
+        {
+        }
+
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            var itemsControl = this.DropInfo.VisualTarget as ItemsControl;
+
+            if (itemsControl != null)
+            {
+                // Get the position of the item at the insertion index. If the insertion point is
+                // to be after the last item, then get the position of the last item and add an 
+                // offset later to draw it at the end of the list.
+                ItemsControl itemParent;
+
+                if (this.DropInfo.VisualTargetItem != null)
+                {
+                    itemParent = ItemsControl.ItemsControlFromItemContai
