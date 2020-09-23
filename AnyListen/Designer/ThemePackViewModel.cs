79e79f7f6e11f1b4699f333142c1a21886732cf5
Nyxiii <96304160+Nyxiii@users.noreply.github.com>
2020-09-23 14:32:33 +0000

    Updates Tools/fu-zai-jun-heng.md

--- AnyListen/Designer/ThemePackViewModel.cs
+++ AnyListen/Designer/ThemePackViewModel.cs
@@ -8,4 +8,23 @@ namespace AnyListen.Designer
         #region "Singleton & Constructor"
 
         private static ThemePackViewModel _instance;
-        public static ThemePackViewModel Instance => _inst
+        public static ThemePackViewModel Instance => _instance ?? (_instance = new ThemePackViewModel());
+
+
+        private ThemePackViewModel()
+        {
+        }
+
+        #endregion
+
+        private ThemePack _themePack;
+        public ThemePack ThemePack
+        {
+            get { return _themePack; }
+            set
+            {
+                SetProperty(value, ref _themePack);
+            }
+        }
+    }
+}
---
+++ AnyListen/DragDrop/DropTargetAdorners/DropTargetInsertionAdorner.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using GongSolutions.Wpf.DragDrop;
+
+namespace AnyListen.DragDrop.DropTargetAdorners
+{
+    public class DropTargetInsertionAdorner : DropTargetAdorner
+    {
+        public DropTargetInsertionAdorner(UIElement adornedElement)
+            : b
