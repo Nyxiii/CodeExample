3bb0af395570f85d87ff48fb3587d4ce67c9e3ce
Nyxiii <96304160+Nyxiii@users.noreply.github.com>
2020-10-11 11:37:11 +0000

    Update scala-library to 2.13.6

--- AnyListen/GUI/Behaviors/AudioVisualisationBehavior.cs
+++ AnyListen/GUI/Behaviors/AudioVisualisationBehavior.cs
@@ -24,4 +24,13 @@ namespace AnyListen.GUI.Behaviors
 
         public static IAudioVisualisationPlugin GetAudioVisualisationForAdvancedWindow(DependencyObject element)
         {
-            return (IAudioVisualisationPlugin
+            return (IAudioVisualisationPlugin)element.GetValue(AudioVisualisationForAdvancedWindowProperty);
+        }
+
+
+        public static readonly DependencyProperty AudioVisualisationForSmartWindowProperty = DependencyProperty.RegisterAttached(
+            "AudioVisualisationForSmartWindow", typeof(IAudioVisualisationPlugin), typeof(AudioVisualisationBehavior), new PropertyMetadata(default(IAudioVisualisationPlugin), AudioVisualisationForSmartWindowChanged));
+
+        private static void AudioVisualisationForSmartWindowChanged(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
+        {
+            AudioVisualisationChanged(dependencyObject, dependencyPropertyChangedEven
