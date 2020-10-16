bcbef98caa8fbcbd6a9a69204478110fa8f12ba9
Nyxiii <96304160+Nyxiii@users.noreply.github.com>
2020-10-16 11:02:26 +0000

    Fixed messages

--- AnyListen/GUI/Behaviors/ContentControlBehavior.cs
+++ AnyListen/GUI/Behaviors/ContentControlBehavior.cs
@@ -20,4 +20,21 @@ namespace AnyListen.GUI.Behaviors
             if (contentControl == null) return;
 
             var textBlock = new TextBlock();
-            var values = GetFormatVal
+            var values = GetFormatValues(contentControl);
+
+            int counter = 0;
+            foreach (var item in Regex.Split(dependencyPropertyChangedEventArgs.NewValue.ToString(), @"\{.\}"))
+            {
+                textBlock.Inlines.Add(new Run(item));
+
+                if (values.Length - 1 < counter) continue;
+                string[] sSplit = values[counter].Split('$');
+                string text = sSplit[0];
+                string url = sSplit[1];
+
+                var hyperlink = new Hyperlink(new Run(text)) { NavigateUri = new Uri(url) };
+                hyperlink.RequestNavigate += (s, e) => { Process.Start(e.Uri.AbsoluteUri); };
+                textBlock.Inlines.Add(hyperlink);
+                counter++;
+            }
+   
