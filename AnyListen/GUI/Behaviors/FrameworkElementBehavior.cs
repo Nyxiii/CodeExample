757d15956eff62d916b49ac89353b05814974e84
Nyxiii <96304160+Nyxiii@users.noreply.github.com>
2020-10-20 16:11:11 +0000

    Added 2 tweets

--- AnyListen/GUI/Behaviors/FrameworkElementBehavior.cs
+++ AnyListen/GUI/Behaviors/FrameworkElementBehavior.cs
@@ -8,4 +8,15 @@ namespace AnyListen.GUI.Behaviors
     {
         public static readonly DependencyProperty AnimationTriggerProperty = DependencyProperty.RegisterAttached(
             "AnimationTrigger", typeof (object), typeof (FrameworkElementBehavior),
-            new PropertyMetadata(default(object), PropertyChangedCallba
+            new PropertyMetadata(default(object), PropertyChangedCallback));
+
+        private static void PropertyChangedCallback(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
+        {
+            var control = (FrameworkElement)dependencyObject;
+            Storyboard storyboard = new Storyboard();
+            DoubleAnimation da = new DoubleAnimation(0.3, 1, TimeSpan.FromMilliseconds(500));
+            ThicknessAnimation ta = new ThicknessAnimation(new Thickness(-10, 0, 10, 0), new Thickness(0), TimeSpan.FromSeconds(0.4));
+            Storyboard.SetTarget(da, control);
+            Storyboard.SetTarget(ta, control);
+            Storyboard.SetTargetProperty(da, new PropertyPath(UIElement.OpacityProperty));
+            Storyboard.SetTargetProperty(ta, new
