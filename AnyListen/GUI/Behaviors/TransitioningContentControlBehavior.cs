d8cb0e3a4ff15fb5b30bfbc0878b22a8d1ee4ec7
Nyxiii <96304160+Nyxiii@users.noreply.github.com>
2020-10-29 09:04:02 +0000

    Change tabs to spaces

--- AnyListen/GUI/Behaviors/TransitioningContentControlBehavior.cs
+++ AnyListen/GUI/Behaviors/TransitioningContentControlBehavior.cs
@@ -1,2 +1,18 @@
 ﻿using System;
-using System.Windo
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using MahApps.Metro.Controls;
+
+namespace AnyListen.GUI.Behaviors
+{
+    class TransitioningContentControlBehavior
+    {
+        public static readonly DependencyProperty DisplayTextProperty = DependencyProperty.RegisterAttached(
+            "DisplayText", typeof (object), typeof (TransitioningContentControlBehavior),
+            new PropertyMetadata(default(object), DisplayTextPropertyChangedCallback));
+
+        private static void DisplayTextPropertyChangedCallback(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
+        {
+            var co
