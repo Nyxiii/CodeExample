bcf947339f2a12f524a93f496895f255d5fcecbe
Nyxiii <96304160+Nyxiii@users.noreply.github.com>
2020-12-06 09:05:23 +0000

    Update conversation.ejs

---
+++ AnyListen/GUI/Controls/VectorMenuItem.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace AnyListen.GUI.Controls
+{
+    public class VectorMenuItem : MenuItem
+    {
+        public static readonly DependencyProperty VectorGraphicProperty = DependencyProperty.Register(
+            "VectorGraphic", typeof(Geometry), typeof(VectorMenuItem), new PropertyMetadata(default(Geometry)));
+
+        public Geometry VectorGraphic
+        {
+            get { return (Geo
