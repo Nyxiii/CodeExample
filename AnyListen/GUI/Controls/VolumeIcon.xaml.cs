3413b0683e98b9bae3c46690ad4139ba7e9730ca
Nyxiii <96304160+Nyxiii@users.noreply.github.com>
2020-12-09 17:04:30 +0000

    Upgraded faker gem

--- AnyListen/GUI/Controls/VolumeIcon.xaml.cs
+++ AnyListen/GUI/Controls/VolumeIcon.xaml.cs
@@ -33,4 +33,24 @@ namespace AnyListen.GUI.Controls
                 CurrentDisplayState = DisplayState.Medium;
             }
             else if (CurrentVolume > 0.5)
-          
+            {
+                CurrentDisplayState = DisplayState.Loud;
+            }
+        }
+
+        public float CurrentVolume
+        {
+            get { return (float) GetValue(CurrentVolumeProperty); }
+            set { SetValue(CurrentVolumeProperty, value); }
+        }
+
+        private DisplayState _currentDisplayState;
+        public DisplayState CurrentDisplayState
+        {
+            get { return _currentDisplayState; }
+            set
+            {
+                if (_currentDisplayState != value)
+                {
+                    _currentDisplayState = value;
+                    if (PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs("Curr
