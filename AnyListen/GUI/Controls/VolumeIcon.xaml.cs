56fa492c6718a2f0fabb975bc6aafd850ae69edb
Nyxiii <96304160+Nyxiii@users.noreply.github.com>
2020-12-09 15:56:19 +0000

    Comments updated

--- AnyListen/GUI/Controls/VolumeIcon.xaml.cs
+++ AnyListen/GUI/Controls/VolumeIcon.xaml.cs
@@ -14,4 +14,23 @@ namespace AnyListen.GUI.Controls
             RefreshCurrentState();
         }
 
-        public static readonly DependencyProperty CurrentVolumeProperty = DependencyProper
+        public static readonly DependencyProperty CurrentVolumeProperty = DependencyProperty.Register(
+            "CurrentVolume", typeof (float), typeof (VolumeIcon), new PropertyMetadata(1.0f, PropertyChangedCallback));
+
+        private static void PropertyChangedCallback(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
+        {
+            ((VolumeIcon) dependencyObject).RefreshCurrentState();
+        }
+
+        void RefreshCurrentState()
+        {
+            if (CurrentVolume == 0)
+            {
+                CurrentDisplayState = DisplayState.Mute;
+            }
+            else if (CurrentVolume <= 0.5)
+            {
+                CurrentDisplayState = DisplayState.Medium;
+            }
+            else if (CurrentVolume > 0.5)
+          
