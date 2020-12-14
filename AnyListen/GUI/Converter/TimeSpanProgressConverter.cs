68262039ef5858562d363db101fc5647a1b0aaf5
Nyxiii <96304160+Nyxiii@users.noreply.github.com>
2020-12-14 13:01:59 +0000

    added missing files

--- AnyListen/GUI/Converter/TimeSpanProgressConverter.cs
+++ AnyListen/GUI/Converter/TimeSpanProgressConverter.cs
@@ -9,4 +9,15 @@ namespace AnyListen.GUI.Converter
     {
         public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
         {
-            if (values[0] == DependencyProperty.UnsetValue || values[1] == DependencyProperty.UnsetValue
+            if (values[0] == DependencyProperty.UnsetValue || values[1] == DependencyProperty.UnsetValue) return 0;
+            var newValue = (TimeSpan)values[0];
+            var totallength = (TimeSpan)values[1];
+            return newValue.TotalSeconds / totallength.TotalSeconds;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AnyListen/GUI/Converter/TrueFalseValueConverter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace AnyListen.GUI.Converter
+{
+    class TrueFalseValueConverter : IValueConverter
+    {
+        public object TrueValue { get; set; }
+        public object FalseValue { get; set; }
+
+        public object Convert(object value, Type targetType, obje
