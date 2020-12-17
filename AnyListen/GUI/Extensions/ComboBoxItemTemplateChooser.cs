8a805b588746c5a5f1be30817ac877b79686c943
Nyxiii <96304160+Nyxiii@users.noreply.github.com>
2020-12-17 13:07:39 +0000

    Added makefile

--- AnyListen/GUI/Extensions/ComboBoxItemTemplateChooser.cs
+++ AnyListen/GUI/Extensions/ComboBoxItemTemplateChooser.cs
@@ -31,4 +31,25 @@ namespace AnyListen.GUI.Extensions
         public static DependencyProperty DropDownTemplateProperty =
             DependencyProperty.RegisterAttached("DropDownTemplate",
                 typeof (DataTemplate),
-                typeof (
+                typeof (ComboBoxItemTemplateChooser),
+                new UIPropertyMetadata(null));
+
+        [AttachedPropertyBrowsableForType(typeof(ComboBox))]
+        public static DataTemplate GetDropDownTemplate(ComboBox obj)
+        {
+            return (DataTemplate)obj.GetValue(DropDownTemplateProperty);
+        }
+        public static void SetDropDownTemplate(ComboBox obj, DataTemplate value)
+        {
+            obj.SetValue(DropDownTemplateProperty, value);
+        }
+
+        #endregion // DropDownTemplate
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            ComboBox parentComboBox;
+            var comboBoxItem = container.GetVisualParent<ComboBoxItem>();
+            if (comboBoxItem == null)
+            {
+ 
