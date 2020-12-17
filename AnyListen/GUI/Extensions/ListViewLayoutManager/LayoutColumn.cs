d8847335866167a7b7c4a278ebd91b58bd0f1391
Nyxiii <96304160+Nyxiii@users.noreply.github.com>
2020-12-17 21:28:30 +0000

    bump development version

--- AnyListen/GUI/Extensions/ListViewLayoutManager/LayoutColumn.cs
+++ AnyListen/GUI/Extensions/ListViewLayoutManager/LayoutColumn.cs
@@ -1,4 +1,26 @@
 ﻿// -- FILE ------------------------------------------------------------------
 // name       : LayoutColumn.cs
 // created    : Jani Giannoudis - 2008.03.27
-// langu
+// language   : c#
+// environment: .NET 3.0
+// copyright  : (c) 2008-2012 by Itenso GmbH, Switzerland
+// --------------------------------------------------------------------------
+
+using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace AnyListen.GUI.Extensions.ListViewLayoutManager
+{
+
+    // ------------------------------------------------------------------------
+    public abstract class LayoutColumn
+    {
+
+        // ----------------------------------------------------------------------
+        protected static bool HasPropertyValue(GridViewColumn column, DependencyProperty dp)
+        {
+            if (column == null)
+            {
+                throw new ArgumentNullException("column");
+     
