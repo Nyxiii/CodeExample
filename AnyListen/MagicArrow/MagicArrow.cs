3e9f1afd670415bdbb84fa221bacc3f2868eb649
Nyxiii <96304160+Nyxiii@users.noreply.github.com>
2021-01-30 15:02:54 +0000

    Updated the spacy feedstock.

--- AnyListen/MagicArrow/MagicArrow.cs
+++ AnyListen/MagicArrow/MagicArrow.cs
@@ -1,4 +1,25 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Threading.Tasks;
-usi
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media.Animation;
+using AnyListen.MagicArrow.DockManager;
+using AnyListen.Settings;
+using AnyListen.Utilities;
+using Cursor = System.Windows.Forms.Cursor;
+
+namespace AnyListen.MagicArrow
+{
+    /// <summary>
+    /// A class for the arrow that is displayed when the cursor hits the right Windows border.
+    /// </summary>
+    public class MagicArrow : IDisposable
+    {
+        private readonly WindowHider _windowHider;
+        private ActiveWindowHook _activewindowhook;
+        private Side _movedOutSide;
+        private StrokeWindow _strokeWindow;
+        private bool _magicArrowIsShown;
+        private bool _isInZone;
+        private bool _mouseWas
