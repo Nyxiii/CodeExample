d505201b87c2ffc1a3d65ef7d050e0337689610f
Nyxiii <96304160+Nyxiii@users.noreply.github.com>
2021-03-03 13:16:08 +0000

    README updated

--- AnyListen/MagicArrow/StrokeWindow.xaml.cs
+++ AnyListen/MagicArrow/StrokeWindow.xaml.cs
@@ -14,4 +14,26 @@ namespace AnyListen.MagicArrow
         public StrokeWindow(double height, double left, double top, Side side)
         {
             InitializeComponent();
-            
+            StrokeWidth = 3;
+            Top = top;
+            SetLeft(left, side);
+            Height = height;
+            SetValue(RenderOptions.EdgeModeProperty, EdgeMode.Aliased);
+            CurrentSide = side;
+            if (ShowLines)
+            {
+                Opacity = 1;
+                Background = Brushes.Red;
+            }
+
+            SourceInitialized += StrokeWindow_SourceInitialized;
+        }
+
+        public Side CurrentSide { get; set; }
+        public double StrokeWidth { get; set; }
+        public bool IsInvisible { get; set; }
+        public static bool ShowLines { get; set; }
+
+        public void SetLeft(double left, Side side)
+        {
+            if (side == Side.Left) { Left = left - (20 - StrokeWidth); } else { Left = left - S
