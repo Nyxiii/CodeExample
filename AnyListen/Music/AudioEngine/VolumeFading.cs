3265a770610bf943225ccf190a1001404a1661bc
Nyxiii <96304160+Nyxiii@users.noreply.github.com>
2021-03-04 19:57:58 +0000

    Update installer.php

---
+++ AnyListen/Music/AudioEngine/VolumeFading.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using CSCore.SoundOut;
+
+namespace AnyListen.Music.AudioEngine
+{
+    public class VolumeFading : IDisposable
+    {
+        public bool IsFading { get; set; }
+        private bool _cancel;
+        public double OutDuration { get; set; }
+        private readonly AutoResetEvent _c
