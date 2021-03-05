0b05b0d40e2dbb0807e0b3dab450e68a88f65c7f
Nyxiii <96304160+Nyxiii@users.noreply.github.com>
2021-03-05 11:30:13 +0000

    Coq dev, repo extra-dev, mode clean.

--- AnyListen/Music/AudioEngine/VolumeFading.cs
+++ AnyListen/Music/AudioEngine/VolumeFading.cs
@@ -10,4 +10,21 @@ namespace AnyListen.Music.AudioEngine
         public bool IsFading { get; set; }
         private bool _cancel;
         public double OutDuration { get; set; }
-        private readonly AutoResetEvent _c
+        private readonly AutoResetEvent _canceledWaiter;
+
+        protected async Task Fade(float from, float to, TimeSpan duration, bool getLouder, ISoundOut soundout)
+        {
+            IsFading = true;
+            float different = Math.Abs(to - from);
+            float step = different / ((float)duration.TotalMilliseconds / 20);
+            float currentvolume = from;
+
+            for (int i = 0; i < duration.TotalMilliseconds / 20; i++)
+            {
+                if (_cancel) { _cancel = false; OnCancelled(); break; }
+                await Task.Delay(20);
+                if (getLouder) { currentvolume += step; } else { currentvolume -= step; }
+                if (currentvolume < 0 || currentvolume > 1) break;
+                try
+                {
+                    soun
