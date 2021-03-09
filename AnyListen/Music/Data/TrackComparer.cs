bbaf6823b10ec108eec7eaaba733b9bf8d74f971
Nyxiii <96304160+Nyxiii@users.noreply.github.com>
2021-03-09 07:19:33 +0000

    Fix messages

--- AnyListen/Music/Data/TrackComparer.cs
+++ AnyListen/Music/Data/TrackComparer.cs
@@ -1,1 +1,21 @@
-﻿using System.Collectio
+﻿using System.Collections.Generic;
+using AnyListen.Music.Track;
+
+namespace AnyListen.Music.Data
+{
+    class TrackComparer : IEqualityComparer<PlayableBase>
+    {
+        public Dictionary<string, string> FileHashes { get; set; }
+        public bool Equals(PlayableBase x, PlayableBase y)
+        {
+            if (x == null || y == null) return false; //would crash if it needs to compute the hash
+            return x.Equals(y);
+        }
+
+        public int GetHashCode(PlayableBase track)
+        {
+            //Check whether the object is null 
+            if (ReferenceEquals(track, null)) return 0;
+
+            //Get hash code for the Title field if it is not null. 
+            int hashProductName = track.Title == null ? 0
