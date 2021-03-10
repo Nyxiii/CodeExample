a06fd393d0c9639f8006f04dc355512eb2bbd3ad
Nyxiii <96304160+Nyxiii@users.noreply.github.com>
2021-03-10 06:05:14 +0000

    :memo: Update issue and PR templates

--- AnyListen/Music/Download/DownloadEntry.cs
+++ AnyListen/Music/Download/DownloadEntry.cs
@@ -1,1 +1,30 @@
-﻿using AnyListen.Music.Tr
+﻿using AnyListen.Music.Track;
+using AnyListen.ViewModelBase;
+
+namespace AnyListen.Music.Download
+{
+    public class DownloadEntry : PropertyChangedBase, IDownloadable
+    {
+        public DownloadSettings DownloadSettings { get; set; }
+
+        private bool _isWaiting;
+        public bool IsWaiting
+        {
+            get { return _isWaiting; }
+            set
+            {
+                SetProperty(value, ref _isWaiting);
+            }
+        }
+        
+        private double _progress;
+        public double Progress
+        {
+            get { return _progress; }
+            set
+            {
+                SetProperty(value, ref _progress);
+            }
+        }
+        
+        private bool _isDown
