7da36a2ec6d0d9a046347d4d452880789367892b
Nyxiii <96304160+Nyxiii@users.noreply.github.com>
2021-03-11 20:01:08 +0000

    Ignore more generated files.

--- AnyListen/Music/Download/DownloadProgress.cs
+++ AnyListen/Music/Download/DownloadProgress.cs
@@ -3,4 +3,9 @@
     class DownloadProgress
     {
         public double Progress { get; set; }
-        public string MegaByte
+        public string MegaBytesDownloaded { get; set; }
+        public string TotalBytesToDownload { get; set; }
+        public string TimeLeft { get; set; }
+        public string DownloadSpeed { get; set; }
+    }
+}
---
+++ AnyListen/Music/Download/DownloadSettings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+using AnyListen.Music.Track;
+
+namespace AnyListen.Music.Download
+{
+    public class DownloadSettings
+    {
+        public bool AddTags { get; set; }
+
+        private string _downloadFolder;
+        [XmlIgnore]
+        public string DownloadFolder
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(_downloadFolder) || !Directory.Exists(_downloadFolder))
+                    return Environment.GetFolderPath(Environment.SpecialFolder.MyMusic);
+
+                return _downloadFolder;
+            }
