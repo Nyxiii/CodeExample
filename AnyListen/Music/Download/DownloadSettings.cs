75edd535f5f380453949fca2ee63755a17195f86
Nyxiii <96304160+Nyxiii@users.noreply.github.com>
2021-03-12 14:50:42 +0000

    Regex fix

--- AnyListen/Music/Download/DownloadSettings.cs
+++ AnyListen/Music/Download/DownloadSettings.cs
@@ -20,3 +20,28 @@ namespace AnyListen.Music.Download
 
                 return _downloadFolder;
             }
+            set { _downloadFolder = value; }
+        }
+
+        [XmlElement(ElementName = "DownloadFolder")]
+        public string DownloadFolder2Serialize
+        {
+            get { return _downloadFolder; }
+            set { _downloadFolder = value; }
+        }
+
+
+        public bool IsConverterEnabled { get; set; }
+        public AudioBitrate Bitrate { get; set; }
+        public AudioFormat Format { get; set; }
+
+        public string GetExtension(IDownloadable track)
+        {
+            return GetExtension(DownloadManager.GetExtension(track));
+        }
+
+        public string GetExtension(string defaultExtension)
+        {
+            switch (Format)
+            {
+                cas
