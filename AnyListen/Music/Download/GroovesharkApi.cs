00a108de794e0923741d06c3f3f558d12d2b9538
Nyxiii <96304160+Nyxiii@users.noreply.github.com>
2021-03-13 21:41:41 +0000

    Day 10

--- AnyListen/Music/Download/GroovesharkApi.cs
+++ AnyListen/Music/Download/GroovesharkApi.cs
@@ -13,4 +13,29 @@ namespace AnyListen.Music.Download
         public class HttpHeaders
         {
             [JsonProperty("Accept-Charset")]
-            public string AcceptCharset { get; s
+            public string AcceptCharset { get; set; }
+
+            [JsonProperty("Content-Length")]
+            public int ContentLength { get; set; }
+
+            [JsonProperty("Accept-Language")]
+            public string AcceptLanguage { get; set; }
+
+            [JsonProperty("Accept-Encoding")]
+            public string AcceptEncoding { get; set; }
+
+            public string Cookie { get; set; }
+
+            [JsonProperty("Content-Type")]
+            public string ContentType { get; set; }
+
+            public string Accept { get; set; }
+
+            [JsonProperty("User-Agent")]
+            public string UserAgent { get; set; }
+        }
+
+        public class GroovesharkResult
+        {
+            [JsonProperty("display_id")]
+            public string Displ
