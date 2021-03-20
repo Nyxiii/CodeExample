e349a5ac62caa54c29d0b0ed21325130724203f2
Nyxiii <96304160+Nyxiii@users.noreply.github.com>
2021-03-20 13:37:02 +0000

    Authentication

--- AnyListen/Music/Download/ffmpeg.cs
+++ AnyListen/Music/Download/ffmpeg.cs
@@ -18,4 +18,21 @@ namespace AnyListen.Music.Download
         /// <param name="settings"></param>
         public static Task ConvertFile(string fileName, string newFileName, DownloadSettings settings)
         {
-            return 
+            return ConvertFile(fileName, newFileName, settings.Bitrate, settings.Format);
+        }
+
+        /// <summary>
+        /// Converts the file
+        /// </summary>
+        /// <param name="fileName">The path to the file which should become converted</param>
+        /// <param name="newFileName">The name of the new file WITHOUT extension</param>
+        /// <param name="bitrate">The audio bitrate</param>
+        /// <param name="format"></param>
+        public static async Task ConvertFile(string fileName, string newFileName, AudioBitrate bitrate, AudioFormat format)
+        {
+            var fileToConvert = new FileInfo(fileName);
+
+            var p = new Process
+            {
+                StartInfo =
+   
