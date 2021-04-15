a8efe74f88bb57e42e294d42b6c14181f13784ac
Nyxiii <96304160+Nyxiii@users.noreply.github.com>
2021-04-15 22:26:00 +0000

    [release] Updated versions after release

--- AnyListen/Music/Playlist/CueSharp.cs
+++ AnyListen/Music/Playlist/CueSharp.cs
@@ -1191,3 +1191,214 @@ namespace CueSharp
             case "CDI/2352":
                 m_TrackDataType = DataType.CDI_2352;
                 break;
+            default:
+                m_TrackDataType = DataType.AUDIO;
+                break;
+            }
+
+            m_TrackFlags = new Flags[0];
+            m_Songwriter = "";
+            m_Title = "";
+            m_ISRC = "";
+            m_Performer = "";
+            m_Indices = new Index[0];
+            m_Garbage = new string[0];
+            m_Comments = new string[0];
+            m_PreGap = new Index(-1, 0, 0, 0);
+            m_PostGap = new Index(-1, 0, 0, 0);
+            m_DataFile = new AudioFile();
+        }
+
+        public Track(int tracknumber, DataType datatype)
+        {
+            m_TrackNumber = tracknumber;
+            m_TrackDataType = datatype;
+
+            m_TrackFlags = new Flags[0];
+            m_Songwriter = "";
+            m_Title = "";
+            m_ISRC = "";
+            m_Performer = "";
+            m_Indices = new Index[0];
+            m_Garbage = new string[0];
+            m_Comments = new string[0];
+            m_PreGap = new Index(-1, 0, 0, 0);
+            m_PostGap = new Index(-1, 0, 0, 0);
+            m_DataFile = new AudioFile();
+        }
+
+        #endregion
+
+        #region Methods
+        public void AddFlag(Flags flag)
+        {
+            //if it's not a none tag
+            //and if the tags hasn't already been added
+            if (flag != Flags.NONE && NewFlag(flag) == true)
+            {
+                m_TrackFlags = (Flags[])CueSheet.ResizeArray(m_TrackFlags, m_TrackFlags.Length + 1);
+                m_TrackFlags[m_TrackFlags.Length - 1] = flag;
+            }
+        }
+
+        public void AddFlag(string flag)
+        {
+            switch (flag.Trim().ToUpper())
+            {
+            case "DATA":
+                AddFlag(Flags.DATA);
+                break;
+            case "DCP":
+                AddFlag(Flags.DCP);
+                break;
+            case "4CH":
+                AddFlag(Flags.CH4);
+                break;
+            case "PRE":
+                AddFlag(Flags.PRE);
+                break;
+            case "SCMS":
+                AddFlag(Flags.SCMS);
+                break;
+            default:
+                return;
+            }
+        }
+
+        public void AddGarbage(string garbage)
+        {
+            if (garbage.Trim() != "")
+            {
+                m_Garbage = (string[])CueSheet.ResizeArray(m_Garbage, m_Garbage.Length + 1);
+                m_Garbage[m_Garbage.Length - 1] = garbage;
+            }
+        }
+
+        public void AddComment(string comment)
+        {
+            if (comment.Trim() != "")
+            {
+                m_Comments = (string[])CueSheet.ResizeArray(m_Comments, m_Comments.Length + 1);
+                m_Comments[m_Comments.Length - 1] = comment;
+            }
+        }
+
+        public void AddIndex(int number, int minutes, int seconds, int frames)
+        {
+            m_Indices = (Index[])CueSheet.ResizeArray(m_Indices, m_Indices.Length + 1);
+
+            m_Indices[m_Indices.Length - 1] = new Index(number, minutes, seconds, frames);
+        }
+
+        public void RemoveIndex(int indexIndex)
+        {
+            for (int i = indexIndex; i < m_Indices.Length - 1; i++)
+            {
+                m_Indices[i] = m_Indices[i + 1];
+            }
+            m_Indices = (Index[])CueSheet.ResizeArray(m_Indices, m_Indices.Length - 1);
+        }
+
+        public TimeSpan Offset
+        {
+            get
+            {
+                if (m_Indices.Length == 0)
+                    return TimeSpan.Zero;
+
+                var index = Indices.Length > 1 ? Indices[1] : Indices[0];
+                return new TimeSpan(0, 0, index.Minutes, index.Seconds, index.Frames * 1000 / 75);
+            }
+        }
+
+        /// <summary>
+        /// Checks if the flag is indeed new in this track.
+        /// </summary>
+        /// <param name="flag">The new flag to be added to the track.</param>
+        /// <returns>True if this flag doesn't already exist.</returns>
+        private bool NewFlag(Flags new_flag)
+        {
+            foreach (Flags flag in m_TrackFlags)
+            {
+                if (flag == new_flag)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder output = new StringBuilder();
+
+            //write file
+            if (m_DataFile.Filename != null && m_DataFile.Filename.Trim() != "")
+            {
+                output.Append("FILE \"" + m_DataFile.Filename.Trim() + "\" " + m_DataFile.Filetype.ToString() + Environment.NewLine);
+            }
+
+            output.Append("  TRACK " + m_TrackNumber.ToString().PadLeft(2, '0') + " " + m_TrackDataType.ToString().Replace('_', '/'));
+
+            //write comments
+            foreach (string comment in m_Comments)
+            {
+                output.Append(Environment.NewLine + "    REM " + comment);
+            }
+
+            if (m_Performer.Trim() != "")
+            {
+                output.Append(Environment.NewLine + "    PERFORMER \"" + m_Performer + "\"");
+            }
+
+            if (m_Songwriter.Trim() != "")
+            {
+                output.Append(Environment.NewLine + "    SONGWRITER \"" + m_Songwriter + "\"");
+            }
+
+            if (m_Title.Trim() != "")
+            {
+                output.Append(Environment.NewLine + "    TITLE \"" + m_Title + "\"");
+            }
+
+            //write flags
+            if (m_TrackFlags.Length > 0)
+            {
+                output.Append(Environment.NewLine + "    FLAGS");
+            }
+
+            foreach (Flags flag in m_TrackFlags)
+            {
+                output.Append(" " + flag.ToString().Replace("CH4", "4CH"));
+            }
+
+            //write isrc
+            if (m_ISRC.Trim() != "")
+            {
+                output.Append(Environment.NewLine + "    ISRC " + m_ISRC.Trim());
+            }
+
+            //write pregap
+            if (m_PreGap.Number != -1)
+            {
+                output.Append(Environment.NewLine + "    PREGAP " + m_PreGap.Minutes.ToString().PadLeft(2, '0') + ":" + m_PreGap.Seconds.ToString().PadLeft(2, '0') + ":" + m_PreGap.Frames.ToString().PadLeft(2, '0'));
+            }
+
+            //write Indices
+            for (int j = 0; j < m_Indices.Length; j++)
+            {
+                output.Append(Environment.NewLine + "    INDEX " + this[j].Number.ToString().PadLeft(2, '0') + " " + this[j].Minutes.ToString().PadLeft(2, '0') + ":" + this[j].Seconds.ToString().PadLeft(2, '0') + ":" + this[j].Frames.ToString().PadLeft(2, '0'));
+            }
+
+            //write postgap
+            if (m_PostGap.Number != -1)
+            {
+                output.Append(Environment.NewLine + "    POSTGAP " + m_PostGap.Minutes.ToString().PadLeft(2, '0') + ":" + m_PostGap.Seconds.ToString().PadLeft(2, '0') + ":" + m_PostGap.Frames.ToString().PadLeft(2, '0'));
+            }
+
+            return output.ToString();
+        }
+
+        #endregion Methods
+    }
+}
---
+++ AnyListen/Music/Playlist/FavoriteList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using AnyListen.Music.Track;
+
+namespace AnyListen.Music.Playlist
+{
+   public class FavoriteList : PlaylistBase
+    {
+       public void Initalize(IEnumerable<NormalPlaylist> playlists)
+       {
+           foreach (var playlist in playlists)
+           {
+               foreach (var track in playlist.Tracks.Where(track => track.IsFavorite))
+                   Tracks.Add(track);
+           }
+       }
+
+       public override void A
