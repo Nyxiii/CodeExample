76f14db751e4e722cc6b5515ee32e611667f6326
Nyxiii <96304160+Nyxiii@users.noreply.github.com>
2021-04-22 09:35:40 +0000

    Removing whitespace

--- AnyListen/Music/Proxy/ProxyManager.cs
+++ AnyListen/Music/Proxy/ProxyManager.cs
@@ -43,4 +43,11 @@ namespace AnyListen.Music.Proxy
         {
             var result = new List<ProxyEntry>();
 
-            using (var wc = new WebClient { P
+            using (var wc = new WebClient { Proxy = null })
+            {
+                var content = await wc.DownloadStringTaskAsync("http://www.cool-proxy.net/proxies/http_proxy_list/sort:score/direction:desc");
+                var regex = new Regex(@"document.write\(Base64.decode\(str_rot13\(""(?<ip>(.*?))""\)\)\)<\/script><\/td>\s+<td>(?<port>([0-9]+))<\/td>.*?<td>.*?<td>(?<country>(.*?))<\/td>.*?;"">(?<working>([0-9]+))<\/span><\/div><\/td>.*?#.{6};"">(?<responsetime>(.*?))<\/span><\/div><\/td>.*?>(?<speed>([0-9]+))<\/span><\/div><\/td>");
+                result.AddRange(from Match match in regex.Matches(content.Replace("\n", null).Replace("\r", null))
+                    select new ProxyEntry
+                    {
+                        Country = match.Groups["country"].Va
