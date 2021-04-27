33fc801a9d0d7f0fb2d2e53cb2417d1f0c96ccdb
Nyxiii <96304160+Nyxiii@users.noreply.github.com>
2021-04-27 22:44:08 +0000

    test data

--- AnyListen/Notification/NotificationService.cs
+++ AnyListen/Notification/NotificationService.cs
@@ -21,4 +21,22 @@ namespace AnyListen.Notification
         void cscore_TrackChanged(object sender, TrackChangedEventArgs e)
         {
             TrackChanged(e.NewTrack);
-      
+        }
+
+        private Window _lastwindow;
+        private PlayableBase _lasttrack;
+
+        void TrackChanged(PlayableBase newtrack)
+        {
+            ConfigSettings config = AnyListenSettings.Instance.Config;
+            if (config.DisableNotificationInGame && WindowHelper.WindowIsFullscreen(UnsafeNativeMethods.GetForegroundWindow())) return;
+            ShowNotification(newtrack, config.Notification);
+
+            _lasttrack = newtrack;
+        }
+
+        protected void ShowNotification(PlayableBase track, NotificationType type)
+        {
+            ConfigSettings config = AnyListenSettings.Instance.Config;
+            if (config.Notification == NotificationType.None) return;
+            if (_lastwindow !
