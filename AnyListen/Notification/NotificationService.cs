0ba896fd87222d46f70ae350aefd3dd10a7aeed2
Nyxiii <96304160+Nyxiii@users.noreply.github.com>
2021-04-27 06:04:40 +0000

    0.6

---
+++ AnyListen/Notification/NotificationService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Reflection;
+using System.Windows;
+using AnyListen.Music.AudioEngine;
+using AnyListen.Music.CustomEventArgs;
+using AnyListen.Music.Track;
+using AnyListen.Notification.Views;
+using AnyListen.Settings;
+using AnyListen.Utilities;
+using AnyListen.Utilities.Native;
+
+namespace AnyListen.Notification
+{
+    public class NotificationService
+    {
+        public NotificationService(CSCoreEngine cscore)
+        {
+            cscore.TrackChanged += cscore_TrackChanged;
+        }
+
+        void cscore_TrackChanged(object sender, TrackChangedEventArgs e)
+        {
+            TrackChanged(e.NewTrack);
+      
