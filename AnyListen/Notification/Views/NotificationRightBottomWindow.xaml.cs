a09d47b6bb1e4f8315c72db692497993362c0787
Nyxiii <96304160+Nyxiii@users.noreply.github.com>
2021-05-01 16:30:59 +0000

    readme updated

--- AnyListen/Notification/Views/NotificationRightBottomWindow.xaml.cs
+++ AnyListen/Notification/Views/NotificationRightBottomWindow.xaml.cs
@@ -1,4 +1,24 @@
 ﻿using System;
 using System.ComponentModel;
 using System.Threading;
-using System.Wi
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media.Animation;
+using AnyListen.Music.Track;
+
+namespace AnyListen.Notification.Views
+{
+    /// <summary>
+    /// Interaction logic for NotificationRightBottomWindow.xaml
+    /// </summary>
+    public partial class NotificationRightBottomWindow
+    {
+        public NotificationRightBottomWindow(PlayableBase track, TimeSpan timestayopened)
+        {
+            CurrentTrack = track;
+            InitializeComponent();
+            Top = SystemParameters.WorkArea.Height - Height;
+            Width = SystemParameters.WorkArea.Width / 4;
+            Left = SystemParameters.WorkArea.Width - Width;
+            Closing += NotificationRightBottomWindow_Closing;
+     
