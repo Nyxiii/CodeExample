74c485a2fbd0fabede848aeb111901b203fc5550
Nyxiii <96304160+Nyxiii@users.noreply.github.com>
2021-05-02 14:28:24 +0000

    Fix possible crash

---
+++ AnyListen/Notification/Views/NotificationTopWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel;
+using System.Threading;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media.Animation;
+using AnyListen.Music.Track;
+
+namespace AnyListen.Notification.Views
+{
+    /// <summary>
+    /// Interaction logic for NotificationTopWindow.xaml
+    /// </summary>
+    public partial class NotificationTopWindow
+    {
+        public NotificationTopWindow(PlayableBase track, TimeSpan timestayopened)
+        {
+            CurrentTrack = track;
+            InitializeComponent();
+            Width = SystemParameters.WorkArea.Width;
+            MouseMove += NotificationTopWindow_MouseMove;
+            Closing += NotificationTopWindow_Closin
