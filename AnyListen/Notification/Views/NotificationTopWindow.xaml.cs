321194711a30f3c626fef2d05a03c0d69d6f1451
Nyxiii <96304160+Nyxiii@users.noreply.github.com>
2021-05-03 15:24:50 +0000

    White space fixes.

--- AnyListen/Notification/Views/NotificationTopWindow.xaml.cs
+++ AnyListen/Notification/Views/NotificationTopWindow.xaml.cs
@@ -19,4 +19,30 @@ namespace AnyListen.Notification.Views
             InitializeComponent();
             Width = SystemParameters.WorkArea.Width;
             MouseMove += NotificationTopWindow_MouseMove;
-            Closing += NotificationTopWindow_Closin
+            Closing += NotificationTopWindow_Closing;
+            Top = SystemParameters.WorkArea.Top;
+            Thread t = new Thread(() =>
+            {
+                Thread.Sleep(timestayopened);
+                if (!_isClosing) Application.Current.Dispatcher.Invoke(MoveOut);
+            }) { IsBackground = true };
+            t.Start();
+        }
+
+        void NotificationTopWindow_Closing(object sender, CancelEventArgs e)
+        {
+            if (!_isClosing) MoveOut();
+            e.Cancel = !_canClose;
+        }
+
+        private bool _isClosing;
+        private bool _canClose;
+        void NotificationTopWindow_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!_isClosing) MoveOut();
+        }
+
+        void MoveOut()
+        {
+            _isClosing = true;
+          
