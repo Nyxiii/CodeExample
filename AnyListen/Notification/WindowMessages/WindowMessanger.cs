92c1600653e73b46d459d9c09c758389afaa7ef4
Nyxiii <96304160+Nyxiii@users.noreply.github.com>
2021-05-07 17:01:32 +0000

    add debug print

--- AnyListen/Notification/WindowMessages/WindowMessanger.cs
+++ AnyListen/Notification/WindowMessages/WindowMessanger.cs
@@ -1,4 +1,27 @@
 ﻿using System;
 using System.Runtime.InteropServices;
 using System.Windows;
-using System.Windo
+using System.Windows.Interop;
+using AnyListen.Utilities.Native;
+
+namespace AnyListen.Notification.WindowMessages
+{
+    class WindowMessanger
+    {
+        // ReSharper disable once InconsistentNaming
+        public const int WM_OPENMUSICFILE = 0x4A;
+        // ReSharper disable once InconsistentNaming
+        public const int WM_BRINGTOFRONT = 3532;
+
+        public event EventHandler BringWindowToFront;
+        public event EventHandler<PlayTrackEventArgs> PlayMusicFile;
+
+        public WindowMessanger(Window baseWindow)
+        {
+            baseWindow.SourceInitialized += (s,e) =>
+            {
+                var source = PresentationSource.FromVisual(baseWindow) as HwndSource;
+                if (source == null) return;
+                source.AddHook(WndProc);
+            };
+    
