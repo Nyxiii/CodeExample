5e4df28fbac446161e6f207d27ebba0f7dbf1cc9
Nyxiii <96304160+Nyxiii@users.noreply.github.com>
2021-05-11 19:56:54 +0000

    Add latest Ansible version (2.5.4)

--- AnyListen/Properties/AssemblyInfo.cs
+++ AnyListen/Properties/AssemblyInfo.cs
@@ -9,4 +9,18 @@ using System.Resources;
 [assembly: AssemblyTitle("AnyListen")]
 [assembly: AssemblyDescription("The music player with universal searching")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Shel
+[assembly: AssemblyCompany("Shelher")]
+[assembly: AssemblyProduct("AnyListen")]
+[assembly: AssemblyCopyright("Copyright © 2013-2016 Shelher")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf "false" werden die Typen in dieser Assembly unsichtbar 
+// für COM-Komponenten.  Wenn Sie auf einen Typ in dieser Assembly von 
+// COM zugreifen müssen, legen Sie das ComVisible-Attribut für diesen Typ auf "true" fest.
+[assembly: ComVisible(false)]
+
+//Um mit dem Erstellen lokalisierbarer Anwendungen zu beginnen, legen Sie 
+//<UICulture>ImCodeVerwendeteKultur</UICulture> in der .csproj-Datei
+//in einer <PropertyGroup> fest.  Wenn Sie in den Quelldateien beispielsweise Deutsch
+//(Deutschland) verwenden, legen Sie <UICulture> auf \"de
