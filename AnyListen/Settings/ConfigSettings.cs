0a50aae0cf63a7cb21dffe3732e6346dea3a540f
Nyxiii <96304160+Nyxiii@users.noreply.github.com>
2021-08-25 17:54:08 +0000

    Spelling errors

---
+++ AnyListen/Settings/ConfigSettings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Windows;
+using System.Xml.Serialization;
+using CSCore.SoundOut;
+using AnyListen.Music.AudioEngine;
+using AnyListen.Music.Download;
+using AnyListen.Notification;
+using AnyListen.Settings.Themes;
+using MahApps.Metro.Controls;
+
+// ReSharper disable InconsistentNaming
+namespace AnyListen.Settings
+{
+    [Serializable, XmlType(TypeName = "Settings")]
+    public class ConfigSettings : SettingsBase
+    {
+        public const string Filename = "c
