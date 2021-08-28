349f78fee2b6f85885c2fdef01f81ed92e05fb82
Nyxiii <96304160+Nyxiii@users.noreply.github.com>
2021-08-28 14:51:21 +0000

    Automatically merging Transifex resources.

--- AnyListen/Settings/ConfigSettings.cs
+++ AnyListen/Settings/ConfigSettings.cs
@@ -18,4 +18,28 @@ namespace AnyListen.Settings
     [Serializable, XmlType(TypeName = "Settings")]
     public class ConfigSettings : SettingsBase
     {
-        public const string Filename = "c
+        public const string Filename = "config.xml";
+
+        //CSCore
+        public string SoundOutDeviceID { get; set; }
+        public SoundOutMode SoundOutMode { get; set; }
+        public int Latency { get; set; }
+        public bool IsCrossfadeEnabled { get; set; }
+        public int CrossfadeDuration { get; set; }
+
+        //Playback
+        public int WaveSourceBits { get; set; }
+        public int SampleRate { get; set; }
+
+        //Magic Arrow
+        public bool ShowMagicArrowBelowCursor { get; set; }
+
+
+        //Search
+        public string PersonalCode { get; set; }
+        public int DownloadBitrate { get; set; }
+        public int LosslessPrefer { get; set; }
+        public int FileNameFormat { get; set; }
+        public int FileFloderFormat { get; set; }
+
+        
