cfa3d0f9f32e1ae5cbfb3d13cf7a181b02ee005c
Nyxiii <96304160+Nyxiii@users.noreply.github.com>
2021-09-10 09:29:18 +0000

    Improvements.

---
+++ AnyListen/Settings/Converter/GroupConverter.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Data;
+
+namespace AnyListen.Settings.Converter
+{
+    public class GroupConverter : IValueConver
