d1fe1b9caec58f8e9d68175cd490cb4eea562a69
Nyxiii <96304160+Nyxiii@users.noreply.github.com>
2021-09-12 13:32:06 +0000

    Update Ruby version

--- AnyListen/Settings/Converter/SampleRateToIndexConverter.cs
+++ AnyListen/Settings/Converter/SampleRateToIndexConverter.cs
@@ -14,4 +14,26 @@ namespace AnyListen.Settings.Converter
                     return 0; 
                 case 44100:
                     return 1; 
-           
+                case 48000:
+                    return 2; 
+                case 88200:
+                    return 3; 
+                case 96000:
+                    return 4; 
+                case 176400:
+                    return 5; 
+                case 192000:
+                    return 6; 
+            }
+            throw new ArgumentException();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            switch (int.Parse(value.ToString()))
+            {
+                case 0:
+                    return -1; 
+                case 1:
+                    return 44100; 
+                case 2
