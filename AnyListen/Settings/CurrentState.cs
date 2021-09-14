6a0b82387894e9aaa4ad276aa83fe2452acb33ab
Nyxiii <96304160+Nyxiii@users.noreply.github.com>
2021-09-14 06:25:00 +0000

    fixing some errors

--- AnyListen/Settings/CurrentState.cs
+++ AnyListen/Settings/CurrentState.cs
@@ -10,4 +10,21 @@ namespace AnyListen.Settings
     [Serializable]
     public class CurrentState : SettingsBase
     {
-        private const
+        private const string Filename = "current.xml";
+
+        public float Volume { get; set; }
+
+        public long TrackPosition { get; set; }
+        public int LastPlaylistIndex { get; set; }
+        public int LastTrackIndex { get; set; }
+        public int SelectedPlaylist { get; set; }
+        public int SelectedTrack { get; set; }
+        public QueueManager Queue { get; set; }
+        public bool IsLoopEnabled { get; set; }
+        public bool IsShuffleEnabled { get; set; }
+        public EqualizerSettings EqualizerSettings { get; set; }
+        public DockingApplicationState ApplicationState { get; set; }
+        public int MainTabControlIndex { get; set; }
+
+        private bool _equalizerIsOpen;
+        public bool EqualizerIsOp
