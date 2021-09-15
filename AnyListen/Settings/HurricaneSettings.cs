aa62d21c7bdb0e0b9f6ca4feb5aaf04fb385d360
Nyxiii <96304160+Nyxiii@users.noreply.github.com>
2021-09-15 15:16:22 +0000

    Removed old readme

--- AnyListen/Settings/HurricaneSettings.cs
+++ AnyListen/Settings/HurricaneSettings.cs
@@ -22,3 +22,71 @@ namespace AnyListen.Settings
             public readonly string ThemePacksDirectory;
             public readonly string AudioVisualisationsDirectory;
             // ReSharper disable once UnassignedReadonlyField
+            public readonly string FFmpegPath;
+
+            public PathProvider()
+            {
+                if (SaveLocationManager.IsInstalled())
+                {
+                    var appDataDir = new DirectoryInfo(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "AnyListen"));
+                    if (!appDataDir.Exists) appDataDir.Create();
+                    BaseDirectory = appDataDir.FullName;
+                }
+                else
+                {
+                    BaseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+                }
+                CoverDirectory = Path.Combine(BaseDirectory, "AlbumCover");
+
+                var themeDirectory = CheckDirectory(Path.Combine(BaseDirectory, "Themes"));
+                AccentColorsDirectory = CheckDirectory(Path.Combine(themeDirectory, "AccentColors"));
+                AppThemesDirectory = CheckDirectory(Path.Combine(themeDirectory, "AppThemes"));
+                ThemePacksDirectory = CheckDirectory(Path.Combine(themeDirectory, "ThemePacks"));
+                AudioVisualisationsDirectory = CheckDirectory(Path.Combine(themeDirectory, "AudioVisualisations"));
+                FFmpegPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "ffmpeg.exe");
+            }
+        }
+
+        public static PathProvider Paths => _paths ?? (_paths = new PathProvider());
+
+        public static AnyListenSettings Instance => _instance ?? (_instance = new AnyListenSettings());
+
+        public PlaylistSettings Playlists { get; set; }
+        public ConfigSettings Config { get; set; }
+        public CurrentState CurrentState { get; set; }
+
+        public bool IsLoaded { get; set; }
+
+        private static string CheckDirectory(string path)
+        {
+            var folder = new DirectoryInfo(path);
+            if (!folder.Exists) folder.Create();
+            return folder.FullName;
+        }
+
+        public void Load()
+        {
+            ApplicationThemeManager.Instance.Refresh();
+            Playlists = PlaylistSettings.Load(Paths.BaseDirectory);
+            Config = ConfigSettings.Load(Paths.BaseDirectory);
+            CurrentState = CurrentState.Load(Paths.BaseDirectory);
+            IsLoaded = true;
+        }
+
+        public void Save()
+        {
+            if (_isSaving)
+                return;
+            _isSaving = true;
+            //Important if the app gets closed
+            var saveThread = new Thread(() =>
+            {
+                Playlists?.Save(Paths.BaseDirectory);
+                Config?.Save(Paths.BaseDirectory);
+                CurrentState?.Save(Paths.BaseDirectory);
+                _isSaving = false;
+            }) {IsBackground = false, Priority = ThreadPriority.Highest};
+            saveThread.Start();
+        }
+    }
+}
---
+++ AnyListen/Settings/PasswordEntry.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
+
+namespace AnyListen.Settings
+{
+    public class PasswordEntry
+    {
+        public string Id { get; set; }
+        [XmlIgnore, JsonIgnore]
+        public string Field1 { get; set; }
+        [XmlIgnore, JsonIgnore]
+        public string Field2 { get; set; }
+
+        [XmlElement(ElementName = "Field1")]
+        public string Field12Serialize
+        {
+            get { return EncryptString(Field1); }
+            set { Fiel
