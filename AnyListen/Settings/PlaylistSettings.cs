92984cd7c8176967b4c06bafcdfcbc19a88501a3
Nyxiii <96304160+Nyxiii@users.noreply.github.com>
2021-09-18 17:44:42 +0000

    previous

--- AnyListen/Settings/PlaylistSettings.cs
+++ AnyListen/Settings/PlaylistSettings.cs
@@ -9,4 +9,24 @@ namespace AnyListen.Settings
     [Serializable, XmlType(TypeName = "Playlists")]
     public class PlaylistSettings : SettingsBase
     {
-        protected const string 
+        protected const string Filename = "playlists.xml";
+
+        [XmlElement("List")]
+        public ObservableCollection<NormalPlaylist> Playlists { get; set; }
+
+        public override void SetStandardValues()
+        {
+            Playlists = new ObservableCollection<NormalPlaylist> { new NormalPlaylist() { Name = "Default" } };
+        }
+
+        public override void Save(string programPath)
+        {
+            var tempFile = new FileInfo(Path.GetTempFileName());
+            Save<PlaylistSettings>(tempFile.FullName);
+
+            var settingsFile = new FileInfo(Path.Combine(programPath, Filename));
+            if (settingsFile.Exists) settingsFile.Delete();
+            tempFile.MoveTo(settingsFile.FullName);
+        }
+
+        public static PlaylistSettin
