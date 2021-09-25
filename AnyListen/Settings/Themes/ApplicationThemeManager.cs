60ad67fed363817c6c630d3b5d20b3da67030c6d
Nyxiii <96304160+Nyxiii@users.noreply.github.com>
2021-09-25 16:26:44 +0000

    Temp Commit

--- AnyListen/Settings/Themes/ApplicationThemeManager.cs
+++ AnyListen/Settings/Themes/ApplicationThemeManager.cs
@@ -5,4 +5,25 @@ using System.IO;
 using System.Linq;
 using System.Windows;
 using AnyListen.Designer.Data;
-using Any
+using AnyListen.Settings.Themes.AudioVisualisation;
+using AnyListen.Settings.Themes.Visual.AccentColors;
+using AnyListen.Settings.Themes.Visual.AppThemes;
+using MahApps.Metro;
+using AppTheme = AnyListen.Settings.Themes.Visual.AppThemes.AppTheme;
+
+namespace AnyListen.Settings.Themes
+{
+    public class ApplicationThemeManager
+    {
+        #region "Singleton & Constructor"
+
+        private static ApplicationThemeManager _instance;
+        public static ApplicationThemeManager Instance => _instance ?? (_instance = new ApplicationThemeManager());
+
+
+        private ApplicationThemeManager()
+        {
+            _loadedResources = new Dictionary<string, ResourceDictionary>();
+        }
+
+        #endreg
