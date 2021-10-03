1d132839a3782f194817fd8b5ee5c053fc1b225a
Nyxiii <96304160+Nyxiii@users.noreply.github.com>
2021-10-03 15:17:41 +0000

    Organised imports

--- AnyListen/Settings/Themes/AudioVisualisation/AudioVisualisationBase.cs
+++ AnyListen/Settings/Themes/AudioVisualisation/AudioVisualisationBase.cs
@@ -5,4 +5,10 @@ namespace AnyListen.Settings.Themes.AudioVisualisation
 {
     [XmlInclude(typeof(BarAudioVisualisation.BarAudioVisualisation)),
     XmlInclude(typeof(SquareAudioVisualisation.SquareAudioVisualisation)),
-    XmlInclude(typeof(Rec
+    XmlInclude(typeof(RectangleVisualisation.RectangleAudioVisualisation))]
+    public abstract class AudioVisualisationBase : IAudioVisualisationContainer
+    {
+        public abstract IAudioVisualisationPlugin Visualisation { get; }
+        public abstract string Name { get; }
+    }
+}
---
+++ AnyListen/Settings/Themes/AudioVisualisation/BarAudioVisualisation/AdvancedWindowAudioVisualisation.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using AnyListen.PluginAPI.AudioVisualisation;
+using WPFSoundVisualizationLib;
+
+namespace AnyListen.Settings.Themes.AudioVisualisation.BarAudioVisualisation
+{
+    public class AdvancedWindowAudioVisualisation : IAudioVisualisation
+    {
+        private SpectrumAnalyzer _spectrumAnalyzer;
+
+        public void Enable()
+        {
+     
