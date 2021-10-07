53d04a1dfbf59f523a771c4e78ae7af3323780d2
Nyxiii <96304160+Nyxiii@users.noreply.github.com>
2021-10-07 11:33:37 +0000

    Linux 3.0.47

--- AnyListen/Settings/Themes/AudioVisualisation/BarAudioVisualisation/BarAudioVisualisation.cs
+++ AnyListen/Settings/Themes/AudioVisualisation/BarAudioVisualisation/BarAudioVisualisation.cs
@@ -6,4 +6,16 @@ namespace AnyListen.Settings.Themes.AudioVisualisation.BarAudioVisualisation
 {
     public class BarAudioVisualisation : AudioVisualisationBase
     {
-        private IAudioVisual
+        private IAudioVisualisationPlugin _loadedPlugin;
+        public override IAudioVisualisationPlugin Visualisation => _loadedPlugin ?? (_loadedPlugin = new AudioVisualisationPlugin());
+
+        public override string Name => Application.Current.Resources["Bars"].ToString();
+
+        public class AudioVisualisationPlugin : IAudioVisualisationPlugin
+        {
+            private IAudioVisualisation _advancedAudioVisualisation;
+            public IAudioVisualisation AdvancedWindowVisualisation => _advancedAudioVisualisation ??
+                                                                      (_advancedAudioVisualisation = new AdvancedWindowAudioVisualisation());
+
+            private IAudioVisualisation _smartWindowAudioVisualisation;
+            public IAudioVisualisation SmartWindowVisualisation =>
