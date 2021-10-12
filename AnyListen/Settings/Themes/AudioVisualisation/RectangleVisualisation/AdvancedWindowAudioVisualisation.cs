30eb433f3f1ec55c608568313a2e19905750ac0f
Nyxiii <96304160+Nyxiii@users.noreply.github.com>
2021-10-12 11:32:34 +0000

    Some reorganization

--- AnyListen/Settings/Themes/AudioVisualisation/RectangleVisualisation/AdvancedWindowAudioVisualisation.cs
+++ AnyListen/Settings/Themes/AudioVisualisation/RectangleVisualisation/AdvancedWindowAudioVisualisation.cs
@@ -1,1 +1,25 @@
-﻿usi
+﻿using System.Windows;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using AnyListen.PluginAPI.AudioVisualisation;
+using WPFSoundVisualizationLib;
+
+namespace AnyListen.Settings.Themes.AudioVisualisation.RectangleVisualisation
+{
+    public class AdvancedWindowAudioVisualisation : IAudioVisualisation
+    {
+        private SpectrumAnalyzer _spectrumAnalyzer;
+
+        public void Enable()
+        {
+            if (_spectrumAnalyzer != null) _spectrumAnalyzer.RefreshInterval = 20;
+        }
+
+        public void Disable()
+        {
+            if (_spectrumAnalyzer != null) _spectrumAnalyzer.RefreshInterval = int.MaxValue;
+        }
+
+        public void Dispose()
+        {
+            _spectrumA
