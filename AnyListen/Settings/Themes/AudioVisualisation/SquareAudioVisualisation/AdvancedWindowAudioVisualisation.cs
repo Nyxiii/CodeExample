e0c8daaea4c13a5414474c2af881e6679f03788e
Nyxiii <96304160+Nyxiii@users.noreply.github.com>
2021-10-20 14:30:01 +0000

    cache result of operator*

--- AnyListen/Settings/Themes/AudioVisualisation/SquareAudioVisualisation/AdvancedWindowAudioVisualisation.cs
+++ AnyListen/Settings/Themes/AudioVisualisation/SquareAudioVisualisation/AdvancedWindowAudioVisualisation.cs
@@ -6,4 +6,29 @@ using WPFSoundVisualizationLib;
 
 namespace AnyListen.Settings.Themes.AudioVisualisation.SquareAudioVisualisation
 {
-    public class Adva
+    public class AdvancedWindowAudioVisualisation : IAudioVisualisation
+    {
+        private SpectrumAnalyzer _spectrumAnalyzer;
+
+        public void Enable()
+        {
+            if (_spectrumAnalyzer != null) _spectrumAnalyzer.RefreshInterval = 20;
+        }
+
+        public void Disable()
+        {
+            if (_spectrumAnalyzer != null) _spectrumAnalyzer.RefreshInterval = int.MaxValue;
+        }
+
+        public void Dispose()
+        {
+            _spectrumAnalyzer = null;
+        }
+
+        private ISpectrumProvider _spectrumProvider;
+        public ISpectrumProvider SpectrumProvider
+        {
+            set { _spectrumProvider = value; }
+        }
+
+        private Colo
