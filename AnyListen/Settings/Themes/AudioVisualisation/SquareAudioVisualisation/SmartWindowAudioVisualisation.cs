47b7708960e353193da6819ae4fb60beca080d9a
Nyxiii <96304160+Nyxiii@users.noreply.github.com>
2021-10-23 20:49:25 +0000

    saa7134: Fix unlocked snd_pcm_stop() call

--- AnyListen/Settings/Themes/AudioVisualisation/SquareAudioVisualisation/SmartWindowAudioVisualisation.cs
+++ AnyListen/Settings/Themes/AudioVisualisation/SquareAudioVisualisation/SmartWindowAudioVisualisation.cs
@@ -1,3 +1,29 @@
 ﻿using System.Windows;
 using System.Windows.Media;
-using System.W
+using System.Windows.Shapes;
+using AnyListen.PluginAPI.AudioVisualisation;
+using WPFSoundVisualizationLib;
+
+namespace AnyListen.Settings.Themes.AudioVisualisation.SquareAudioVisualisation
+{
+    class SmartWindowAudioVisualisation : IAudioVisualisation
+    {
+        private SpectrumAnalyzer _spectrumAnalyzer;
+
+        public void Enable()
+        {
+            if (_spectrumAnalyzer != null) _spectrumAnalyzer.RefreshInterval = 20;
+        }
+
+        public void Disable()
+        {
+            if (_spectrumAnalyzer != null) _spectrumAnalyzer.RefreshInterval = int.MaxValue;
+        }
+
+        public void Dispose()
+        {
+            _spectrumAnalyzer = null;
+        }
+
+        private ISpectrumProvider _spectrumProvider;
+        public ISpectrumProvider SpectrumPro
