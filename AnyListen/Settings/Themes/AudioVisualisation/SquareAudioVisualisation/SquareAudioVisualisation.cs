db6d4c3e158fe02b471225cb845d5e18db7f2ad0
Nyxiii <96304160+Nyxiii@users.noreply.github.com>
2021-10-27 15:16:38 +0000

    Rustup

--- AnyListen/Settings/Themes/AudioVisualisation/SquareAudioVisualisation/SquareAudioVisualisation.cs
+++ AnyListen/Settings/Themes/AudioVisualisation/SquareAudioVisualisation/SquareAudioVisualisation.cs
@@ -1,2 +1,17 @@
 ﻿using System.Windows;
-using System.
+using System.Windows.Media;
+using AnyListen.PluginAPI.AudioVisualisation;
+
+namespace AnyListen.Settings.Themes.AudioVisualisation.SquareAudioVisualisation
+{
+    public class SquareAudioVisualisation : AudioVisualisationBase
+    {
+        private IAudioVisualisationPlugin _loadedPlugin;
+        public override IAudioVisualisationPlugin Visualisation => _loadedPlugin ?? (_loadedPlugin = new AudioVisualisationPlugin());
+
+        public override string Name => Application.Current.Resources["Squares"].ToString();
+
+        public class AudioVisualisationPlugin : IAudioVisualisationPlugin
+        {
+            private IAudioVisualisation _advancedAudioVisualisation;
+            public IAudioVisualisation AdvancedWindowVisualisation =>
