707bf20a47671a28d269493c5d761a10edc39e81
Nyxiii <96304160+Nyxiii@users.noreply.github.com>
2021-11-03 13:10:13 +0000

    add DS_Store to gitignore

--- AnyListen/Settings/Themes/Background/CustomApplicationBackground.cs
+++ AnyListen/Settings/Themes/Background/CustomApplicationBackground.cs
@@ -5,4 +5,27 @@ using AnyListen.ViewModelBase;
 
 namespace AnyListen.Settings.Themes.Background
 {
-    public class CustomApplicationBackground : PropertyChangedBa
+    public class CustomApplicationBackground : PropertyChangedBase, IApplicationBackground
+    {
+        protected bool Equals(CustomApplicationBackground other)
+        {
+            if (other.BackgroundPath == null && BackgroundPath == null) return true;
+            return string.Equals(BackgroundPath, other.BackgroundPath);
+        }
+
+        public override int GetHashCode()
+        {
+            return (BackgroundPath != null ? BackgroundPath.GetHashCode() : 0);
+        }
+
+        private string _backgroundPath;
+        public string BackgroundPath
+        {
+            get { return _backgroundPath; }
+            set
+            {
+                SetProperty(value, ref _backgroundPath);
+            }
+        }
+
+        public Uri GetB
