ae7a9b2c7352dce76179d058cd691c5fbe514724
Nyxiii <96304160+Nyxiii@users.noreply.github.com>
2021-11-10 21:17:46 +0000

    updating

--- AnyListen/Settings/Themes/Visual/AppThemes/CustomAppTheme.cs
+++ AnyListen/Settings/Themes/Visual/AppThemes/CustomAppTheme.cs
@@ -1,4 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-usin
+using System.Linq;
+using System.Windows;
+using System.Windows.Markup;
+using System.Xml.Serialization;
+
+namespace AnyListen.Settings.Themes.Visual.AppThemes
+{
+    [Serializable]
+    public class CustomAppTheme : AppThemeBase
+    {
+        public static bool FromFile(string filename, out CustomAppTheme result)
+        {
+            var appTheme = new CustomAppTheme { Name = Path.GetFileNameWithoutExtension(filename) };
+
+            try
+            {
+                if (!IsAppThemeDictionary(appTheme.ResourceDictionary))
+                {
+                    result = null;
+                    return false;
+                }
+            }
+            catch (XamlParseException)
+            {
+                result = null;
+                return fals
