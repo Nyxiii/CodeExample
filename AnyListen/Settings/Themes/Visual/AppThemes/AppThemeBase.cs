1ac9811dd54e46d8ce3e4cf997da52d2b3e1c699
Nyxiii <96304160+Nyxiii@users.noreply.github.com>
2021-11-10 14:01:39 +0000

    update version to 0.0.2

--- AnyListen/Settings/Themes/Visual/AppThemes/AppThemeBase.cs
+++ AnyListen/Settings/Themes/Visual/AppThemes/AppThemeBase.cs
@@ -9,4 +9,28 @@ namespace AnyListen.Settings.Themes.Visual.AppThemes
     public abstract class AppThemeBase : IAppTheme, IGroupable
     {
         public string Name { set; get; }
-        public abstract string TranslatedName { get
+        public abstract string TranslatedName { get; }
+        public abstract string Group { get; }
+        [XmlIgnore]
+        public abstract ResourceDictionary ResourceDictionary { get; }
+
+        public override bool Equals(object obj)
+        {
+            var other = obj as AppThemeBase;
+            if (other == null) return false;
+            if (obj.GetType() == GetType())
+                return other.Name == Name;
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public void ApplyTheme()
+        {
+            ApplicationThemeManager.Instance.LoadResource("apptheme", ResourceDictionary);
+        }
+    }
+}
