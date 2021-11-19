bc86b111f24ad9381b4469639eb0ddae74af2f3f
Nyxiii <96304160+Nyxiii@users.noreply.github.com>
2021-11-19 19:26:29 +0000

    ios

--- AnyListen/Utilities/DependencyObjectExtensions.cs
+++ AnyListen/Utilities/DependencyObjectExtensions.cs
@@ -10,4 +10,22 @@ namespace AnyListen.Utilities
     {
         public static T GetVisualParent<T>(this DependencyObject child) where T : Visual
         {
-            while ((child != null) && !(child is
+            while ((child != null) && !(child is T))
+            {
+                child = VisualTreeHelper.GetParent(child);
+            }
+            return (T) child;
+        }
+
+        private static bool IsUserVisible(FrameworkElement element, FrameworkElement container)
+        {
+            if (!element.IsVisible)
+                return false;
+
+            Rect bounds =
+                element.TransformToAncestor(container).TransformBounds(new Rect(0.0, 0.0, element.ActualWidth, element.ActualHeight));
+            var rect = new Rect(0.0, 0.0, container.ActualWidth, container.ActualHeight);
+            return rect.Contains(bounds.TopLeft) || rect.Contains(bounds.BottomRight);
+        }
+
+        public static List<ListBoxItem> GetVisibleItemsFromItemsControl(Item
