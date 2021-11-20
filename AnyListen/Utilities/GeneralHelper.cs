9ae91c88469599a3fbbcd5569a28a0120af9cd3d
Nyxiii <96304160+Nyxiii@users.noreply.github.com>
2021-11-20 12:29:48 +0000

    Fix: typo

--- AnyListen/Utilities/GeneralHelper.cs
+++ AnyListen/Utilities/GeneralHelper.cs
@@ -1,4 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using Syst
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnyListen.Utilities
+{
+    public static class GeneralHelper
+    {
+        /// <summary>
+        /// Function to get file impression in form of string from a file location.
+        /// </summary>
+        /// <param name="filename">File Path to get file impression.</param>
+        /// <returns>Byte Array</returns>
+        // ReSharper disable once InconsistentNaming
+        public static string FileToMD5Hash(string filename)
+        {
+            using (var md5 = MD5.Create())
+            {
+                using (var stream = File.OpenRead(fi
