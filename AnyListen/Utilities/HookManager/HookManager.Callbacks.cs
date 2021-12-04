4ce226323f61f32339d17b9df7696d249a9b11f6
Nyxiii <96304160+Nyxiii@users.noreply.github.com>
2021-12-04 19:22:30 +0000

    Update changelog and readme

--- AnyListen/Utilities/HookManager/HookManager.Callbacks.cs
+++ AnyListen/Utilities/HookManager/HookManager.Callbacks.cs
@@ -1,1 +1,19 @@
-﻿using Syst
+﻿using System;
+using System.ComponentModel;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace AnyListen.Utilities.HookManager
+{
+    public static partial class HookManager
+    {
+        /// <summary>
+        /// The CallWndProc hook procedure is an application-defined or library-defined callback 
+        /// function used with the SetWindowsHookEx function. The HOOKPROC type defines a pointer 
+        /// to this callback function. CallWndProc is a placeholder for the application-defined 
+        /// or library-defined function name.
+        /// </summary>
+        /// <param name="nCode">
+        /// [in] Specifies whether the hook procedure must process the message. 
+  
