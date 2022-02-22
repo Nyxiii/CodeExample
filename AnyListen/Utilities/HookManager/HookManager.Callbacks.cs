4df86e5d9a65bad32737a98a567f6eb57007037e
Nyxiii <96304160+Nyxiii@users.noreply.github.com>
2022-02-22 20:16:54 +0000

    Updating commit reference for src/main/enterprise

--- AnyListen/Utilities/HookManager/HookManager.Callbacks.cs
+++ AnyListen/Utilities/HookManager/HookManager.Callbacks.cs
@@ -421,4 +421,20 @@ namespace AnyListen.Utilities.HookManager
                 int result = UnhookWindowsHookEx(_keyboardHookHandle);
                 //reset invalid handle
                 _keyboardHookHandle = 0;
-   
+                //Free up for GC
+                _keyboardDelegate = null;
+                //if failed and exception must be thrown
+                if (result == 0)
+                {
+                    //Returns the error code returned by the last unmanaged function called using platform invoke that has the DllImportAttribute.SetLastError flag set. 
+                    int errorCode = Marshal.GetLastWin32Error();
+                    //Initializes and throws a new instance of the Win32Exception class with the specified error. 
+                    throw new Win32Exception(errorCode);
+                }
+            }
+        }
+
+        #endregion
+
+    }
+}
