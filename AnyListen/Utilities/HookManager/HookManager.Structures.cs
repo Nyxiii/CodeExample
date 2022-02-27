91ad2d31e446a9d8b360785096fc56de831dfd3b
Nyxiii <96304160+Nyxiii@users.noreply.github.com>
2022-02-27 13:05:23 +0000

    directory structure

--- AnyListen/Utilities/HookManager/HookManager.Structures.cs
+++ AnyListen/Utilities/HookManager/HookManager.Structures.cs
@@ -1,2 +1,23 @@
 ﻿using System.Runtime.InteropServices;
-// ReSharper disable FieldCanBeMadeReadO
+// ReSharper disable FieldCanBeMadeReadOnly.Local
+// ReSharper disable MemberCanBePrivate.Local
+// ReSharper disable once InconsistentNaming
+
+namespace AnyListen.Utilities.HookManager
+{
+    public static partial class HookManager
+    {
+        /// <summary>
+        /// The Point structure defines the X- and Y- coordinates of a point. 
+        /// </summary>
+        /// <remarks>
+        /// http://msdn.microsoft.com/library/default.asp?url=/library/en-us/gdi/rectangl_0tiq.asp
+        /// </remarks>
+        [StructLayout(LayoutKind.Sequential)]
+        private struct Point
+        {
+            /// <summary>
+            /// Specifies the X-coordinate of the point. 
+            /// </summary>
+            public int X;
+            ///
