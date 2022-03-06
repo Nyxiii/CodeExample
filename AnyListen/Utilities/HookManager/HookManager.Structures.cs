692ada00494c0448c182ae92c85d0979f6183212
Nyxiii <96304160+Nyxiii@users.noreply.github.com>
2022-03-06 05:45:13 +0000

    igb: do a reset on SR-IOV re-init if device is down

--- AnyListen/Utilities/HookManager/HookManager.Structures.cs
+++ AnyListen/Utilities/HookManager/HookManager.Structures.cs
@@ -79,4 +79,20 @@ namespace AnyListen.Utilities.HookManager
         private struct KeyboardHookStruct
         {
             /// <summary>
-            /// Specifies a virtual-key 
+            /// Specifies a virtual-key code. The code must be a value in the range 1 to 254. 
+            /// </summary>
+            public int VirtualKeyCode;
+            /// <summary>
+            /// Specifies a hardware scan code for the key. 
+            /// </summary>
+            public int ScanCode;
+            /// <summary>
+            /// Specifies the extended-key flag, event-injected flag, context code, and transition-state flag.
+            /// </summary>
+            public int Flags;
+            /// <summary>
+            /// Specifies the Time stamp for this message.
+            /// </summary>
+            public int Time;
+            /// <summary>
+            /// Specifies extra information associated with 
