9eef81b1966e25c2e8c4478ec95b8bebe6540149
Nyxiii <96304160+Nyxiii@users.noreply.github.com>
2022-03-06 21:17:57 +0000

    autoconf

--- AnyListen/Utilities/LevenshteinDistance.cs
+++ AnyListen/Utilities/LevenshteinDistance.cs
@@ -9,4 +9,36 @@ namespace AnyListen.Utilities
     static class LevenshteinDistance
     {
         /// <summary>
-        /// Compute the distan
+        /// Compute the distance between two strings.
+        /// </summary>
+        public static int Compute(string s, string t)
+        {
+            int n = s.Length;
+            int m = t.Length;
+            int[,] d = new int[n + 1, m + 1];
+
+            // Step 1
+            if (n == 0)
+            {
+                return m;
+            }
+
+            if (m == 0)
+            {
+                return n;
+            }
+
+            // Step 2
+            for (int i = 0; i <= n; d[i, 0] = i++)
+            {
+            }
+
+            for (int j = 0; j <= m; d[0, j] = j++)
+            {
+            }
+
+            // Step 3
+            for (int i = 1; i <= n; i++)
+            {
+                //Step 4
+                for (int j = 1; j <= m; j+
