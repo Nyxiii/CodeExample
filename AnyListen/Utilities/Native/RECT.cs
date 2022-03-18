fb550a91ec6128ef75a86bfce2a08eb8a808c3a1
Nyxiii <96304160+Nyxiii@users.noreply.github.com>
2022-03-18 15:36:56 +0000

    gofmt fix

--- AnyListen/Utilities/Native/RECT.cs
+++ AnyListen/Utilities/Native/RECT.cs
@@ -9,4 +9,33 @@ namespace AnyListen.Utilities.Native
     {
         public int left;
         public int top;
- 
+        public int right;
+        public int bottom;
+
+        public static readonly RECT Empty;
+
+        public int Width => Math.Abs(right - left);
+
+        public int Height => bottom - top;
+
+        public RECT(int left, int top, int right, int bottom)
+        {
+            this.left = left;
+            this.top = top;
+            this.right = right;
+            this.bottom = bottom;
+        }
+
+        public RECT(RECT rcSrc)
+        {
+            left = rcSrc.left;
+            top = rcSrc.top;
+            right = rcSrc.right;
+            bottom = rcSrc.bottom;
+        }
+
+        public bool IsEmpty => left >= right || top >= bottom;
+
+        public override string ToString()
+        {
+      
