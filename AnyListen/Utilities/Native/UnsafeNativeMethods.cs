bdd6a2cdbdbf1640815f6a7dfff3b7e168f76cf9
Nyxiii <96304160+Nyxiii@users.noreply.github.com>
2022-03-20 18:24:40 +0000

    msm: vidc: Initialize kernel space stack variables

--- AnyListen/Utilities/Native/UnsafeNativeMethods.cs
+++ AnyListen/Utilities/Native/UnsafeNativeMethods.cs
@@ -28,4 +28,19 @@ namespace AnyListen.Utilities.Native
         [DllImport("user32.dll")]
         internal static extern IntPtr GetShellWindow();
 
-        [DllImport("user32.dll", CharSe
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        internal static extern bool EnumWindows(EnumedWindow lpEnumFunc, ArrayList lParam);
+
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        internal static extern int GetClassName(IntPtr hWnd, StringBuilder lpClassName, int nMaxCount);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        internal static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        internal static extern IntPtr SendMessage(IntPtr hWnd, UInt32 Msg, IntPtr wParam, IntPtr lParam);
+
+        /// <summary>
+        /// The FindWindow function ret
