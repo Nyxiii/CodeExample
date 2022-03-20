ea0880e552fdd8efe3f3244518388dbf55d24d9b
Nyxiii <96304160+Nyxiii@users.noreply.github.com>
2022-03-20 11:03:53 +0000

    5.4.3

--- AnyListen/Utilities/Native/RECT.cs
+++ AnyListen/Utilities/Native/RECT.cs
@@ -56,4 +56,16 @@ namespace AnyListen.Utilities.Native
             return left.GetHashCode() + top.GetHashCode() + right.GetHashCode() + bottom.GetHashCode();
         }
 
-        public static bool operator ==(RE
+        public static bool operator ==(RECT rect1, RECT rect2)
+        {
+            return (rect1.left == rect2.left && rect1.top == rect2.top && rect1.right == rect2.right && rect1.bottom == rect2.bottom);
+        }
+
+        public static bool operator !=(RECT rect1, RECT rect2)
+        {
+            return !(rect1 == rect2);
+        }
+
+
+    }
+}
---
+++ AnyListen/Utilities/Native/UnsafeNativeMethods.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Runtime.InteropServices;
+using System.Security;
+using System.Text;
+// ReSharper disable InconsistentNaming
+
+namespace AnyListen.Utilities.Native
+{
+    internal static class UnsafeNativeMethods
+    {
+        [DllImport("user32.dll")]
+        internal static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll")]
+        internal static extern bool UnhookWinEvent(IntPt
