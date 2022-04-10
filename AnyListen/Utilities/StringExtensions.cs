ca9f444689fe4ce858a72a5ebb8b2fb66ed8786d
Nyxiii <96304160+Nyxiii@users.noreply.github.com>
2022-04-10 15:16:00 +0000

    times

--- AnyListen/Utilities/StringExtensions.cs
+++ AnyListen/Utilities/StringExtensions.cs
@@ -1,4 +1,22 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
-using System.W
+using System.Web;
+
+namespace AnyListen.Utilities
+{
+    public static class StringExtensions
+    {
+        public static string ToSentenceCase(this string s)
+        {
+            return Regex.Replace(s, "[a-z][A-Z]", m => m.Value[0] + " " + char.ToUpper(m.Value[1]));
+        }
+
+        /// <summary>
+        /// Remove all invalid chars for a file name
+        /// </summary>
+        /// <param name="fileNameToEscape">The name of the file which could contain invalid chars</param>
+        /// <returns>The <see cref="fileNameToEscape"/> without the invalid chars</returns>
+        public static string ToEscapedFilename(this string fileNameToEscape)
+        {
+            char[] illegalchars
