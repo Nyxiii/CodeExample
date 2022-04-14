893f59eee86f0a8287f1fbdad659dd54f377cc6d
Nyxiii <96304160+Nyxiii@users.noreply.github.com>
2022-04-14 10:46:56 +0000

    new setup

--- AnyListen/ViewModelBase/RelayCommand.cs
+++ AnyListen/ViewModelBase/RelayCommand.cs
@@ -11,4 +11,33 @@ namespace AnyListen.ViewModelBase
     {
         #region "Declarations"
         public delegate void ExecuteDelegate(object parameter);
-   
+        private readonly Func<bool> _canExecute;
+        #endregion
+        private readonly ExecuteDelegate _execute;
+
+        #region "Constructors"
+        public RelayCommand(ExecuteDelegate execute)
+            : this(execute, null)
+        {
+        }
+
+        public RelayCommand(ExecuteDelegate execute, Func<bool> canExecute)
+        {
+            if (execute == null)
+            {
+                throw new ArgumentNullException("execute");
+            }
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+        #endregion
+
+        #region "ICommand"
+        public event EventHandler CanExecuteChanged
+        {
+
+            add
+            {
+                if (_canExecute != null)
+                {
+         
