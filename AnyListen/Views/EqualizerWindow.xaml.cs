e037cea90fed9cceca2d68941c4c6a7456da7e6c
Nyxiii <96304160+Nyxiii@users.noreply.github.com>
2022-06-04 20:56:19 +0000

    isdnloop: Validate NUL-terminated strings from user.

--- AnyListen/Views/EqualizerWindow.xaml.cs
+++ AnyListen/Views/EqualizerWindow.xaml.cs
@@ -8,4 +8,28 @@ using AnyListen.Utilities.Native;
 namespace AnyListen.Views
 {
     /// <summary>
-    /// Interacti
+    /// Interaction logic for EqualizerWindow.xaml
+    /// </summary>
+    public partial class EqualizerWindow
+    {
+        public event EventHandler BeginCloseAnimation;
+
+        public EqualizerWindow(RECT rect, double width)
+        {
+            InitializeComponent();
+            SetPosition(rect, width);
+            Closing += EqualizerWindow_Closing;
+        }
+
+        bool _isClosing;
+        bool _canClose;
+        void EqualizerWindow_Closing(object sender, CancelEventArgs e)
+        {
+            if (!_canClose)
+            {
+                e.Cancel = true;
+                if (!_isClosing)
+                {
+                    _isClosing = true;
+                    Storyboard story = new Storyboard();
+     
