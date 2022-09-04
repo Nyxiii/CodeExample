2bc4591990c44acf5d9619e6aafa321b43fa46c5
Nyxiii <96304160+Nyxiii@users.noreply.github.com>
2022-09-04 14:43:42 +0000

    fix pagination

--- AnyListen/Views/Tools/LanguageCreatorWindow.xaml.cs
+++ AnyListen/Views/Tools/LanguageCreatorWindow.xaml.cs
@@ -5,4 +5,29 @@ using System.IO;
 using System.Linq;
 using System.Windows;
 using AnyListen.ViewModelBase;
-using Micr
+using Microsoft.Win32;
+
+namespace AnyListen.Views.Tools
+{
+    /// <summary>
+    /// Interaction logic for QueueManagerWindow.xaml
+    /// </summary>
+    public partial class LanguageCreatorWindow : INotifyPropertyChanged
+    {
+        public LanguageCreatorWindow()
+        {
+            InitializeComponent();
+        }
+
+        #region Properties
+
+        private LanguageDocument _currentLanguageDocument;
+        public LanguageDocument CurrentLanguageDocument
+        {
+            get { return _currentLanguageDocument; }
+            set { _currentLanguageDocument = value; OnPropertyChanged("CurrentLanguageDocument"); }
+        }
+
+        private string _filePath;
+        public string FilePath
+     
