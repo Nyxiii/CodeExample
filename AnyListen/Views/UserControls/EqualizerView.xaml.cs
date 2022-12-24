7f59b9b43b3c067b0cbbbaceeba85ad9a34b363f
Nyxiii <96304160+Nyxiii@users.noreply.github.com>
2022-12-24 11:05:43 +0000

    cpufreq: interactive: turn boost_pulse off on boost off

---
+++ AnyListen/Views/UserControls/EqualizerView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Input;
+
+namespace AnyListen.Views.UserControls
+{
+    /// <summary>
+    /// Interaction logic for EqualizerView.xaml
+    /// </summary>
+    public partial class EqualizerView : INotifyPropertyChanged
+    {
+        public event EventHandler WantClose;
+        public Thickness SliderThickness { get; set; }
+       
