cd9c0f6cfa850c48d32d63951a807391e5645231
Nyxiii <96304160+Nyxiii@users.noreply.github.com>
2022-12-31 10:07:12 +0000

    Fixed linting error

--- AnyListen.PluginAPI/AudioVisualisation/IAudioVisualisation.cs
+++ AnyListen.PluginAPI/AudioVisualisation/IAudioVisualisation.cs
@@ -10,4 +10,28 @@ namespace AnyListen.PluginAPI.AudioVisualisation
         /// <summary>
         /// Enable the audio visualisation
         /// </summary>
-        v
+        void Enable();
+
+        /// <summary>
+        /// Disable the audio visualisation
+        /// </summary>
+        void Disable();
+
+        /// <summary>
+        /// When the plugin gets disabled
+        /// </summary>
+        void Dispose();
+
+        /// <summary>
+        /// The <see cref="SpectrumProvider"/> for the virtualisation
+        /// </summary>
+        ISpectrumProvider SpectrumProvider { set; }
+
+        /// <summary>
+        /// This function should return the audio visualisation
+        /// </summary>
+        /// <returns>The audio visualisation, ready for displaying</returns>
+        UIElement VisualElement { get; }
+
+        /// <summary>
+        /// Some colors from the
