9a53e32db769442d8bc863b4f87e035d31939150
Nyxiii <96304160+Nyxiii@users.noreply.github.com>
2023-01-01 13:59:54 +0000

    Latest wheels build by travis-ci. [ci skip]

--- AnyListen.PluginAPI/AudioVisualisation/ISpectrumProvider.cs
+++ AnyListen.PluginAPI/AudioVisualisation/ISpectrumProvider.cs
@@ -10,4 +10,22 @@ namespace AnyListen.PluginAPI.AudioVisualisation
         /// <summary>
         /// Assigns current FFT data to a buffer.
         /// </summary>
-        /// <param name="fftDataBuffer">The buffer to copy FFT d
+        /// <param name="fftDataBuffer">The buffer to copy FFT data.</param>
+        /// <returns>True if data was written to the buffer, otherwise false.</returns>
+        bool GetFFTData(float[] fftDataBuffer);
+
+        /// <summary>
+        /// Gets the index in the FFT data buffer for a given frequency.
+        /// </summary>
+        /// <param name="frequency">The frequency for which to obtain a buffer index</param>
+        /// <returns>An index in the FFT data buffer</returns>
+        int GetFFTFrequencyIndex(int frequency);
+
+        /// <summary>
+        /// Gets whether the sound player is currently playing audio.
+        /// </summary>
+        bool IsPlaying { get; }
+
+        /// <summary>
+        /// When <see cref="IsPlaying"/> is changed
+  
