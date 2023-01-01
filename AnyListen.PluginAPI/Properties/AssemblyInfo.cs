884309e52e6410199f19671829c1c5a37c95b93a
Nyxiii <96304160+Nyxiii@users.noreply.github.com>
2023-01-01 17:49:33 +0000

    Delete old files

--- AnyListen.PluginAPI/Properties/AssemblyInfo.cs
+++ AnyListen.PluginAPI/Properties/AssemblyInfo.cs
@@ -14,4 +14,15 @@ using System.Windows;
 [assembly: AssemblyProduct("AnyListen.PluginAPI")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyC
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf "false" werden die Typen in dieser Assembly unsichtbar 
+// für COM-Komponenten.  Wenn Sie auf einen Typ in dieser Assembly von 
+// COM zugreifen müssen, legen Sie das ComVisible-Attribut für diesen Typ auf "true" fest.
+[assembly: ComVisible(false)]
+
+//Um mit dem Erstellen lokalisierbarer Anwendungen zu beginnen, legen Sie 
+//<UICulture>ImCodeVerwendeteKultur</UICulture> in der .csproj-Datei
+//in einer <PropertyGroup> fest.  Wenn Sie in den Quelldateien beispielsweise Deutsch
+//(Deutschland) verwenden, legen Sie <UICulture> auf \"de-DE\" fest.  Heben Sie dann die Auskommentierung
+//des nachstehenden NeutralResourceLanguage-Attributs 
