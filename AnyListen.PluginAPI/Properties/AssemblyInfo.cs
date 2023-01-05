b348bf07a85b87a004d6bff180f565ed57f7137f
Nyxiii <96304160+Nyxiii@users.noreply.github.com>
2023-01-05 19:04:06 +0000

    Updated makefile

--- AnyListen.PluginAPI/Properties/AssemblyInfo.cs
+++ AnyListen.PluginAPI/Properties/AssemblyInfo.cs
@@ -25,4 +25,15 @@ using System.Windows;
 //<UICulture>ImCodeVerwendeteKultur</UICulture> in der .csproj-Datei
 //in einer <PropertyGroup> fest.  Wenn Sie in den Quelldateien beispielsweise Deutsch
 //(Deutschland) verwenden, legen Sie <UICulture> auf \"de-DE\" fest.  Heben Sie dann die Auskommentierung
-//des nachstehenden NeutralResourceLanguage-Attributs 
+//des nachstehenden NeutralResourceLanguage-Attributs auf.  Aktualisieren Sie "en-US" in der nachstehenden Zeile,
+//sodass es mit der UICulture-Einstellung in der Projektdatei übereinstimmt.
+
+//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
+
+
+[assembly:ThemeInfo(
+    ResourceDictionaryLocation.None, //Speicherort der designspezifischen Ressourcenwörterbücher
+                             //(wird verwendet, wenn eine Ressource auf der Seite 
+                             // oder in den Anwendungsressourcen-Wörterbüchern nicht gefunden werden kann.)
+    ResourceDictionaryLocation.SourceAssembly //Speicherort des generischen Ressourcenwörterbuchs
+                                      //(wird verwendet, wenn eine Ressource auf 
